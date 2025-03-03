9789029371d9e3b21a48c8e3f88908aa44578c66
temaantonov11 <temaantonov11@yandex.ru>
2025-03-04 01:48:29 +0300

    add createUser functional without setting role

--- databaseAPI/DataBase.py
+++ databaseAPI/DataBase.py
@@ -1,33 +1,37 @@
 import psycopg2
+import psycopg2.sql
 import logger
 from .config import host, port, user, dbname, password
 
 class DataBase:
-    
-    def __init__(self, username, role):
-        self.__username = username
-        self.__role = role
-        self.__connection = self.__connect()
-    
+     
     def __del__(self):
         self.__connection.close()
         logger.log.info("[POSTGRESQL]: Connection closed.")
-        
-
-    def _create():
-        pass
     
-    def __connect(self):
+
+    def createUser(self, username, role, passwd):
         try:
-            connection = psycopg2.connect(
+            self.__cursor.execute(psycopg2.sql.SQL("CREATE USER {} WITH PASSWORD %s;").format(psycopg2.sql.Identifier(username)), [passwd])
+            self.__user = username
+            self.__password = passwd
+            self.__role = role
+            logger.log.info(f"[POSTGRESQL]: User {self.__user} successfull created.")
+        except psycopg2.Error as _ex:
+            logger.log.error("[POSTGRESQL]: User did not create")
+            
+    
+    def connect(self):
+        try:
+            self.__connection = psycopg2.connect(
                 dbname=dbname,
                 user=user,
                 password=password,
                 host=host,
                 port=port
             )
-            connection.autocommit = True
+            self.__connection.autocommit = True
+            self.__cursor = self.__connection.cursor()
             logger.log.info("[POSTGRESQL]: Connection opened.")
-            return connection
-        except Exception as _ex:
+        except psycopg2.Error as _ex:
             logger.log.error("[POSTGRESQL]: Connection error", _ex)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from databaseAPI import *
 
-new_db = DataBase("tema", "superuser")
+new_db = DataBase()
+new_db.connect()
+new_db.createUser("temaantonov11", "superuser", "123")
 
 
