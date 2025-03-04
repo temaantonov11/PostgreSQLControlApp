8dcdfcb6082edc6edc68cd2d19465662da2b05b8
temaantonov11 <temaantonov11@yandex.ru>
2025-03-04 15:59:52 +0300

    + create DataBase

--- databaseAPI/DataBase.py
+++ databaseAPI/DataBase.py
@@ -3,22 +3,29 @@ import psycopg2.sql
 from logger import *
 from .config import host, port, user, dbname, password
 
-
-
 class DataBase:
      
+    def __init__(self, user=user, passwd=password):
+        self.__user = user
+        self.__password = passwd
+    
     def __del__(self):
-        self.__connection.close()
-        log.info("[POSTGRESQL]: Connection closed.")
+        self.__disconnect()
     
-    
-    def initUser(self, username, role, passwd):
+
+    def initUser(self, username, role, passwd, status, dbName):
         try:
-            self.__createUser(username, role, passwd)
-            self.__setRole(username, role)
+            if status == "REGISTRY":
+                self.connect(dbName)
+                self.__createUser(username, role, passwd)
+                self.__setRole(username, role)
+                self.__disconnect()
+            
             self.__user = username
             self.__password = passwd
             self.__role = role
+            self.connect(dbName)
+            
         except psycopg2.Error as _ex:
             log.error("[POSTGRESQL]: User did not init", _ex)
             
@@ -44,18 +51,33 @@ class DataBase:
         except psycopg2.Error as _ex:
             log.error("[POSTGRESQL]: Error when granting admin/guest rights", _ex)
 
-    
-    def connect(self):
+    def createDataBase(self, newDbName):
+        try:
+            self.__cursor.execute(psycopg2.sql.SQL("CREATE DATABASE {};").format(psycopg2.sql.Identifier(newDbName)))
+            self.__disconnect()
+            self.connect(newDbName)
+            log.info(f"[POSTGRESQL]: DB {newDbName} created.")
+        except psycopg2.Error as _ex:
+            log.error(f"[POSTGRESQL]: Failed create {newDbName} DB.\n {_ex}")
+    def connect(self, db = dbname):
         try:
             self.__connection = psycopg2.connect(
                 dbname=dbname,
-                user=user,
-                password=password,
+                user=self.__user,
+                password=self.__password,
                 host=host,
                 port=port
             )
             self.__connection.autocommit = True
             self.__cursor = self.__connection.cursor()
-            log.info("[POSTGRESQL]: Connection opened.")
+            log.info(f"[POSTGRESQL]: {self.__user} connected to {db}.")
         except psycopg2.Error as _ex:
-            log.error("[POSTGRESQL]: Connection error", _ex)
+            log.error("[POSTGRESQL]: Connection error {}".format(_ex))
+
+    def __disconnect(self):
+        try:
+            self.__connection.close()
+            self.__cursor.close()
+            log.info(f"[POSTGRESQL]: {self.__user} disconnected.")
+        except psycopg2.Error as _ex:
+            log.error("[POSTGRESQL]: disconnection error")
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from databaseAPI import *
 
 new_db = DataBase()
+
 new_db.connect()
-new_db.initUser("dima71", "Admin", "123")
+
+new_db.createDataBase("goods")
+
+new_db.initUser("masik41111", "Admin", "123", "REGISTRY", "goods")
+
 
 
